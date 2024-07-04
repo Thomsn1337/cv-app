09b25fd456dd4cc3f34ddf856ff66baa870be019
Thomas Steifler <steiflerthomas@gmail.com>
2024-07-05 00:12:09 +0200

    feat: add general information to sidebar

--- src/components/cvViewer.jsx
+++ src/components/cvViewer.jsx
@@ -10,6 +10,26 @@ export default function CVViewer({ cvData }) {
                 <p>{general.title}</p>
             </div>
             <div className="cv-side">
+                <section className="general-info">
+                    <ul>
+                        <li>
+                            <FontAwesomeIcon icon="fa-solid fa-calendar" />
+                            {general.dateOfBirth}
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon="fa-solid fa-phone" />
+                            {general.phone}
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon="fa-solid fa-envelope" />
+                            {general.email}
+                        </li>
+                        <li>
+                            <FontAwesomeIcon icon="fa-solid fa-location-dot" />
+                            {general.location}
+                        </li>
+                    </ul>
+                </section>
             </div>
             <div className="cv-content"></div>
         </div>
