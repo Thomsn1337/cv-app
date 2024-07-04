100896d76ec72223b94441a959cbe02262f3a7c7
Thomas Steifler <steiflerthomas@gmail.com>
2024-07-05 00:50:10 +0200

    feat: add simple component to display skill rating bars

---
+++ src/components/skillInfo.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import "../styles/skillInfo.css"
+
+export default function SkillInfo({ name, rating }) {
+    const ratingBlocks = new Array(5).fill(false);
+    for (let i = 0; i < rating; i++) {
+        ratingBlocks[i] = true;
+    }
+
+    return (
+        <>
+            <p>{name}</p>
+            <div className="rating-bar">
+                {ratingBlocks.map((block) => {
+                    return block ? (
+                        <div className="rating-block set"></div>
+                    ) : (
+                        <div className="rating-block"></div>
+                    );
+                })}
+            </div>
+        </>
+    );
+}
