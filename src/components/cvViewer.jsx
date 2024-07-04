7eefc7e364d6ac5c9014c7af38abf3cf5efcb9de
Thomas Steifler <steiflerthomas@gmail.com>
2024-07-05 00:50:40 +0200

    feat: add skills section to sidebar

--- src/components/cvViewer.jsx
+++ src/components/cvViewer.jsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SkillInfo from "./skillInfo";
 import "../styles/cvViewer.css";
 
 export default function CVViewer({ cvData }) {
     const { general, education, experience, skills, languages } = cvData;
+
     return (
         <div className="cv-viewer">
             <div className="cv-header">
@@ -30,6 +33,28 @@ export default function CVViewer({ cvData }) {
                         </li>
                     </ul>
                 </section>
+
+                {skills.length !== 0 && (
+                    <>
+                        <div className="separator"></div>
+                        <section className="skills">
+                            <h3>Skills</h3>
+                            <ul>
+                                {skills.map((skill, index) => {
+                                    console.log(skill);
+                                    return (
+                                        <li key={index}>
+                                            <SkillInfo
+                                                name={skill.name}
+                                                rating={skill.rating}
+                                            />
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </section>
+                    </>
+                )}
             </div>
             <div className="cv-content"></div>
         </div>
