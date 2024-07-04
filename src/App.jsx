cbfd6097756b0b1e482a40629b8da3b2f1f30a58
Thomas Steifler <steiflerthomas@gmail.com>
2024-07-05 00:11:10 +0200

    feat: add fontawesome icon support

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
 import { useState } from "react";
 import CVViewer from "./components/cvViewer";
 import exampleData from "./exampleData";
@@ -11,3 +16,5 @@ export default function App() {
         </>
     );
 }
+
+library.add(fab, fas, far);
