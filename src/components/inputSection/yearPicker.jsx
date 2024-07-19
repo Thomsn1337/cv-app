2f17217a4e0256d8657e615fd571de49a35c7a48
Thomas Steifler <steiflerthomas@gmail.com>
2024-07-19 17:09:45 +0200

    feat: add year picker component

---
+++ src/components/inputSection/yearPicker.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+
+export default function YearPicker({ name, id, range = 50 }) {
+    const currentYear = new Date().getFullYear();
+    const startYear = currentYear - range;
+
+    const years = [];
+    for (let i = currentYear; i >= startYear; i--) {
+        years.push(i);
+    }
+
+    return (
+        <div className="year-picker">
+            <label htmlFor={id}>{name}</label>
+            <select name={id} id={id} defaultValue={currentYear}>
+                {years.map((year, index) => (
+                    <option key={index} value={year}>
+                        {year}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
